695f2bf81604a4cdee7893e730dbb1cac460ed1b
william <william.del.rosario@iceshop.nl>
2019-09-07 13:52:10 +0200

    Find the Vowels

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,53 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const matches = str.match(/[aeiou]/gi);
+
+    return matches ? matches.length : 0;
+}
+
+console.log(vowels('HELLO'));
 
 module.exports = vowels;
+
+// function vowels(str) {
+//     let count = 0;
+//     const checker = 'aeiou'; // or ['a','e','i','o','u']
+//
+//     for (let char of str.toLowerCase()) {
+//         if (checker.includes(char)) {
+//             count++;
+//         }
+//     }
+//
+//     return count;
+// }
+//
+// console.log(vowels('HELLO'));
+
+// function vowels(str) {
+//     let count = 0;
+//
+//     for (let char of str.toLowerCase()) {
+//         if (str.toLowerCase().includes('a')) {
+//             count++
+//         }
+//         if (str.toLowerCase().includes('e')) {
+//             count++
+//         }
+//         if (str.toLowerCase().includes('i')) {
+//             count++
+//         }
+//         if (str.toLowerCase().includes('o')) {
+//             count++
+//         }
+//         if (str.toLowerCase().includes('u')) {
+//             count++
+//         }
+//
+//         return count;
+//     }
+// }
+//
+// console.log(vowels('HELLO'));
