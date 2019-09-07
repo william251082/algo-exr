f2dd380c5196d1715bddc41c4a01b8bf9cb0b1e7
william <william.del.rosario@iceshop.nl>
2019-09-07 13:46:11 +0200

    Anagrams

--- anagrams/index.js
+++ anagrams/index.js
@@ -1,5 +1,5 @@
 // --- Directions
-// Check to see if two provided strings are anagrams of eachother.
+// Check to see if two provided strings are anagrams of each other.
 // One string is an anagram of another if it uses the same characters
 // in the same quantity. Only consider characters, not spaces
 // or punctuation.  Consider capital letters to be the same as lower case
@@ -8,6 +8,78 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB)
+}
+
+function cleanString(str) {
+    return str
+        .replace(/[^\w]/g, "")
+        .toLowerCase()
+        .split('')
+        .sort()
+        .join('');
+}
+
+console.log(anagrams('rail safety', 'fairy tales'));
 
 module.exports = anagrams;
+
+
+// function anagrams(stringA, stringB) {
+//     const strA = stringA.replace(/[^\w]/g, "").toLowerCase();
+//     const strB = stringB.replace(/[^\w]/g, "").toLowerCase();
+//     const charMapA = {};
+//     const charMapB = {};
+//
+//     for (let char of strA) {
+//         charMapA[char] = charMapA[char] + 1 || 1;
+//     }
+//
+//     const orderedA = {};
+//     Object.keys(charMapA).sort().forEach(function(key) {
+//         orderedA[key] = charMapA[key];
+//     });
+//
+//     for (let char of strB) {
+//         charMapB[char] = charMapB[char] + 1 || 1;
+//     }
+//
+//     const orderedB = {};
+//     Object.keys(charMapB).sort().forEach(function(key) {
+//         orderedB[key] = charMapB[key];
+//     });
+//
+//     return JSON.stringify(orderedA) === JSON.stringify(orderedB);
+// }
+//
+// console.log(anagrams('rail safety', 'fairy tales'));
+
+// function anagrams(stringA, stringB) {
+//     const aCharMap = buildCharMaps(stringA);
+//     const bCharMap = buildCharMaps(stringB);
+//
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//         return false;
+//     }
+//
+//     for (let char in aCharMap) {
+//         if (aCharMap[char] !== bCharMap[char]) {
+//             return false;
+//         }
+//     }
+//
+//     return true;
+// }
+//
+// function buildCharMaps(str) {
+//     const charMap = {};
+//
+//     for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//         charMap[char] = charMap[char] + 1 || 1;
+//     }
+//
+//     return charMap;
+// }
+//
+// console.log(anagrams('rail safety', 'fairy tales'));
