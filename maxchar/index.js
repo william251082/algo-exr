4f930629e170100a7afa8d23d9179e2293d25fc8
william <william.del.rosario@iceshop.nl>
2019-09-07 13:42:27 +0200

    MaxChar

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,49 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar;
+}
+
+console.log(maxChar('Hello'));
 
 module.exports = maxChar;
+
+
+// function maxChar(str) {
+//     const charMap = {};
+//     let max = 0;
+//     let maxChar = '';
+//
+//     for (let char of str) {
+//         if (charMap[char]) {
+//             charMap[char]++;   // charMap[char] - will return the string key
+//         } else {
+//             charMap[char] = 1;
+//         }
+//     }
+//
+//     for (let char in charMap) {   // charMap[char] - will return the object key
+//         if (charMap[char] > max){
+//             max = charMap[char];
+//             maxChar = char;
+//         }
+//     }
+//
+//     return maxChar;
+// }
