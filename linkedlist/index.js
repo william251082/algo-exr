ddcf3a5a23f54eecb3bcf5ce4ffc57643c688164
william <william.del.rosario@iceshop.nl>
2020-03-15 14:03:41 +0100

    linked list insert first

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -13,8 +13,19 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+
+    insertFirst(data) {
+        this.head = new Node(data, this.head);;
+    }
 }
 
+//test
+const nodeOne = new Node(5);
 const list = new LinkedList();
-list.head = new Node();
+
+list.head = nodeOne;
+list.insertFirst(15);
+
+console.log(list);
+
 module.exports = { Node, LinkedList };
