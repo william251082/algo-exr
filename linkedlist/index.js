a9f45bc8256859669e18bfefc01cbd0ff131f044
william <william.del.rosario@iceshop.nl>
2020-03-18 11:48:16 +0100

    linked list generators

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -150,23 +150,117 @@ class LinkedList {
         const prev = this.getAt(index - 1) || this.getLast();
         prev.next = new Node(data, prev.next);
     }
+
+    forEach() {
+
+    }
 }
 
 //test
-const nodeOne = new Node('a');
-const list = new LinkedList();
-
-list.head = nodeOne;
-
-list.insertFirst('hi');
-list.insertFirst('b');
-list.insertFirst('c');
-list.insertFirst('d');
-list.insertFirst('e');
-list.insertFirst('f');
-
-console.log(list.insertAt('insertAt', 5));
-console.log(list.getAt(5));
+// const nodeOne = new Node('a');
+// const list = new LinkedList();
+//
+// list.head = nodeOne;
+//
+// list.insertLast(1);
+// list.insertLast(2);
+// list.insertLast(3);
+// list.insertLast(4);
+// list.insertLast(5);
+// list.insertLast(6);
+//
+//
+// console.log(list.forEach((node, index) => {
+//     node.data += 10
+// }));
+// console.log(list.getAt(0));
 
 
 module.exports = { Node, LinkedList };
+
+// function *numbers() {
+//     const result = 1 + 1;
+//     return 20 + (yield result)
+// }
+//
+// // when you define a generator -- it will return {}
+// // and you call .next(), the code inside will execute until yield is found
+// // that value will be on generator.next
+// const generator = numbers();
+// generator.next(); // {"value":2,"done":false}
+// generator.next(); // {"value":null,"done":true}
+// generator.next(10); // {"value":30,"done":true}
+
+
+// function *list() {
+//     yield 1;
+//     yield 2;
+//     yield 3;
+//     yield 4;
+//     yield 5;
+// }
+//
+// const generator = list();
+// generator.next(); // {"value":1,"done":false}
+// generator.next(); // {"value":2,"done":false}
+// generator.next(); // {"value":3,"done":false}
+// generator.next(); // {"value":4,"done":false}
+// generator.next(); // {"value":5,"done":false}
+// generator.next(); // {"done":true}
+// generator.next(); // {"done":true}
+//
+// const numbers = [];
+// for (let value of generator) {
+//     numbers.push(value);
+// }
+
+// function *numbers() {
+//     yield 1;
+//     yield 2;
+//     yield* moreNumbers();
+//     yield 6;
+//     yield 7;
+// }
+//
+// function *moreNumbers() {
+//     yield 3;
+//     yield 4;
+//     yield 5;
+// }
+//
+// const generator = numbers();
+//
+// const values = [];
+// for (let value of generator) {
+//     values.push(value)
+// }
+//
+// console.log(values);
+
+
+class Tree {
+    constructor(value = null, children = []) {
+        this.value = value;
+        this.children = children;
+    }
+
+    *printValues() {
+        yield this.value;
+        for (let child of this.children) {
+            yield* child.printValues();
+        }
+    }
+}
+
+const tree = new Tree(1, [
+    new Tree(2, [new Tree(4)]),
+    new Tree(3)
+]);
+
+
+const values = [];
+for (let value of tree.printValues()) {
+    values.push(value);
+}
+
+console.log(tree);
