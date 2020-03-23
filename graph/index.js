c32be7ac7d3506adb494799fdb64f5e79e35cee1
william <william.del.rosario@iceshop.nl>
2020-03-23 15:24:21 +0100

    adjacency matrix vs. list

--- graph/index.js
+++ graph/index.js
@@ -1,10 +1,5 @@
-// adjacency list
-
-const arr = [
-    [1, 5],
-    [0, 2],
-    [1, 3],
-    [2, 4],
-    [3, 5],
-    [4, 0],
-];
+class Graph {
+    constructor() {
+        this.adjacencyList = {}
+    }
+}
