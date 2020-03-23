94ac1913a9ccc70bd10a01032424b0044befd904
william <william.del.rosario@iceshop.nl>
2020-03-23 08:52:46 +0100

    BST insertion

--- bst/index.js
+++ bst/index.js
@@ -11,6 +11,44 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    insert(data) {
+        // check on left hand side, see algo
+        if (data < this.data && this.left) {
+            this.left.insert(data);
+        } else if (data < this.data) {
+            this.left = new Node(data);
+        // check on right hand side, see algo
+        } else if (data > this.data && this.right) {
+            this.right.insert(data);
+        } else if (data > this.data) {
+            this.right = new Node(data);
+        }
+    }
+}
 
 module.exports = Node;
+
+
+// bst = left child < parent  and right child > parent
+// bt = no restrictions
+
+// assertion checks
+// left and right node and the last
+
+// algo
+// left hand side
+// 1. if n < root node data and no left child,
+// 2. recurse assign as left child
+// 3. handle the case where n < node data and no left child
+//
+// //right hand side
+// 1. if n > root node data and no left child,
+// 2. recurse assign as left child
+// 3. handle the case where n > node data and no left child
