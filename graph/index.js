08e29fbc242f2f43d4a5e111cff9af2e1d51e49e
william <william.del.rosario@iceshop.nl>
2020-03-23 16:02:58 +0100

    removeEdge() implementation

--- graph/index.js
+++ graph/index.js
@@ -10,8 +10,21 @@ class Graph {
     }
 
     addEdge(v1, v2) {
+        // directed graph
         this.adjacencyList[v1] = [v2];
         this.adjacencyList[v2] = [v1];
+        // // undirected graph
+        // this.adjacencyList[v1] = [v2];
+    }
+
+    removeEdge(v1, v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(
+            v => v !== v2
+        );
+
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(
+            v => v !== v1
+        );
     }
 }
 
@@ -20,4 +33,6 @@ g.addVertex('hi');
 g.addVertex('hi1');
 g.addVertex('hi2');
 g.addEdge('hi', 'hi1');
+g.addEdge('hi', 'hi2');
+g.removeEdge('hi', 'hi2');
 console.dir(g);
