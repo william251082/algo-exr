1641bdf2fd132f4d5a74c948657e212c4b5169e9
william <william.del.rosario@iceshop.nl>
2020-03-23 13:17:49 +0100

    merge() implementation

--- sorting/index.js
+++ sorting/index.js
@@ -47,10 +47,21 @@ function selectionSort(arr) {
 
 function mergeSort(arr) {
 
+
 }
 
 function merge(left, right) {
+    const results = [];
 
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift())
+        } else  {
+            results.push(right.shift())
+        }
+    }
+
+    return [...results, ...left, ...right];
 }
 
 function getArray() {
