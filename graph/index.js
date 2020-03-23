491c5e7bad06d73098723595a03ff18e8d8525b4
william <william.del.rosario@iceshop.nl>
2020-03-23 15:31:28 +0100

    addVertex() implementation

--- graph/index.js
+++ graph/index.js
@@ -2,4 +2,16 @@ class Graph {
     constructor() {
         this.adjacencyList = {}
     }
+
+    addVertex(vertex) {
+        if(!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+        }
+    }
 }
+
+const g = new Graph();
+g.addVertex('hi');
+g.addVertex('hi1');
+g.addVertex('hi2');
+console.dir(g);
