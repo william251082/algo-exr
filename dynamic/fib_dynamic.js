764ef64972809a04b181a4ed6d15fda0f5ad0690
william <william.del.rosario@iceshop.nl>
2020-03-24 16:59:45 +0100

    dynamic programming init --fib

---
+++ dynamic/fib_dynamic.js
@@ -0,0 +1,45 @@
+// function fib(n) {
+//     if (n <= 2) {
+//         return 1;
+//     }
+//
+//     return fib(n-1) + fib(n-2);
+// }
+//
+// // performance: O(2N), 1.6 to the N
+// console.log(fib(9));
+
+// // fib memoized
+// function fib(n, memo=[]) {
+//     if (memo[n] !== undefined) {
+//         return memo[n]
+//     }
+//
+//     if (n <= 2) {
+//         return 1;
+//     }
+//
+//     var result = fib(n - 1, memo) + fib(n - 2, memo);
+//
+//     memo[n] = result;
+//
+//     return result;
+// }
+//
+// console.log(fib(9));
+
+// preprogram the basics
+function fib(n, memo=[undefined, 1, 1]) {
+    if (memo[n]) {
+        return memo[n];
+    }
+    if (n <= 2) {
+        return 1;
+    }
+    var result = fib(n - 1) + fib(n - 2);
+    memo[n] = result;
+
+    return result
+}
+
+console.log(fib(100));
