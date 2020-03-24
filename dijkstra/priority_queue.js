10297e8d2aa1eb6a41c429a8b7365f1dc007f162
william <william.del.rosario@iceshop.nl>
2020-03-24 10:26:00 +0100

    priority queue implementation

---
+++ dijkstra/priority_queue.js
@@ -0,0 +1,36 @@
+class PriorityQueue {
+    constructor() {
+        this.values = [];
+    }
+
+    // push ansort object element to value
+    enqueue(val, priority) {
+        this.values.push({val, priority});
+        this.sort();
+    }
+
+    // return the object with lowest priority
+    dequeue() {
+        return this.values.shift();
+    }
+
+    // sort priority
+    sort() {
+        // O(N*log(N))
+        this.values.sort((a, b) => a.priority - b.priority);
+    }
+}
+
+let p = new PriorityQueue();
+
+p.enqueue("A", 6);
+p.enqueue("B", 7);
+p.enqueue("C", 9);
+p.enqueue("D", 2);
+p.enqueue("E", 1);
+p.enqueue("F", 8);
+p.enqueue("G", 3);
+
+console.dir(p.values);
+console.log(p.dequeue());
+
