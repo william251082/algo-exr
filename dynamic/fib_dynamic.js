f0f935b749fd91125941c5977624ee9ba99ae78f
william <william.del.rosario@iceshop.nl>
2020-03-25 08:49:49 +0100

    tabulation --bottom up

--- dynamic/fib_dynamic.js
+++ dynamic/fib_dynamic.js
@@ -29,17 +29,28 @@
 // console.log(fib(9));
 
 // preprogram the basics
-function fib(n, memo=[undefined, 1, 1]) {
-    if (memo[n]) {
-        return memo[n];
-    }
-    if (n <= 2) {
-        return 1;
-    }
-    var result = fib(n - 1) + fib(n - 2);
-    memo[n] = result;
+// function fib(n, memo=[undefined, 1, 1]) {
+//     if (memo[n]) {
+//         return memo[n];
+//     }
+//     if (n <= 2) {
+//         return 1;
+//     }
+//     var result = fib(n - 1) + fib(n - 2);
+//     memo[n] = result;
+//
+//     return result
+// }
+//
+// console.log(fib(100));
 
-    return result
+// tabulated, work from the bottom up
+function fib(n) {
+    if (n <= 2) return 1;
+
+    var fibNums = [0,1,1];
+    for(var i = 3; i <= n; i++) {
+        fibNums[i] = fibNums[i-1] + fibNums[i-2];
+    }
+    return fibNums[n];
 }
-
-console.log(fib(100));
