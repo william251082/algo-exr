5eff78598627bdddc564372fbf53aee7d3dd27ee
william <william.del.rosario@iceshop.nl>
2020-04-03 12:20:17 +0200

    reverse string

--- ds/array.js
+++ ds/array.js
@@ -34,13 +34,48 @@ class MyArray {
     }
 }
 
-const myArray = new MyArray();
-myArray.push('hi');
-myArray.push('you');
-myArray.push('!');
-// myArray.pop();
-myArray.deleteAtIndex(0);
-// myArray.push('are');
-// myArray.push('nice');
-// myArray.shiftItems(0);
-console.log(myArray);
+// const myArray = new MyArray();
+// myArray.push('hi');
+// myArray.push('you');
+// myArray.push('!');
+// // myArray.pop();
+// myArray.deleteAtIndex(0);
+// // myArray.push('are');
+// // myArray.push('nice');
+// // myArray.shiftItems(0);
+// console.log(myArray);
+
+// // reverse string
+// function reverse(str) {
+//     let reversed = '';
+//
+//     for (let char of str) {
+//         reversed = char + reversed
+//     }
+//
+//     return reversed;
+// }
+//
+// console.log(reverse('ih'));
+
+function reverse(str){
+    if(!str || typeof str != 'string' || str.length < 2 ) return str;
+
+    const backwards = [];
+    const totalItems = str.length - 1;
+    for(let i = totalItems; i >= 0; i--){
+        backwards.push(str[i]);
+    }
+    return backwards.join('');
+}
+
+// function reverse2(str){
+//     //check for valid input
+//     return str.split('').reverse().join('');
+// }
+//
+// const reverse3 = str => [...str].reverse().join('');
+//
+// reverse('Timbits Hi')
+// reverse('Timbits Hi')
+// reverse3('Timbits Hi')
