101111b032d86ccfd6ef38d58c63d99a106b1242
william <william.del.rosario@iceshop.nl>
2020-04-03 11:25:01 +0200

    data structures overview

---
+++ ds/index.js
@@ -0,0 +1,14 @@
+// list of data structures
+// https://en.wikipedia.org/wiki/List_of_data_structures
+// RAM and persistent memory
+// https://www.youtube.com/watch?v=fpnE6UAfbtU
+// computer memory
+// http://statmath.wu.ac.at/courses/data-analysis/itdtHTML/node55.html
+
+
+// traversal --access each only once
+// access
+// insertion
+// deletion
+// searching
+// sorting
