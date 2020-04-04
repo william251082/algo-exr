da2228a04a3689919391b88e606b79bec5eebcca
william <william.del.rosario@iceshop.nl>
2020-04-04 13:19:50 +0200

    keys()

--- ds/hash_tables/hash.js
+++ ds/hash_tables/hash.js
@@ -40,14 +40,28 @@ class HashTable {
         }
         return undefined;
     }
+
+    keys() {
+        const keysArray = [];
+        // loop to find the keys --no guaranteed order
+        for (let i=0; i < this.data.length; i++) {
+            if (this.data[i]) {
+                // the bucket
+                console.log('bucket', this.data[i]);
+                // element inside the bucket
+                console.log('element', this.data[i][0]);
+                // key of the first element
+                console.log('key', this.data[i][0][0]);
+                keysArray.push(this.data[i][0][0]);
+            }
+        }
+        return keysArray;
+    }
 }
 
+// for in --slow
 const myHashTable = new HashTable(50);
 myHashTable.set('grapes', 10000);
-myHashTable.set('grapes', 10001);
-// console.log(myHashTable.get('grapes'));
-// myHashTable.set('apples', 9);
-// myHashTable.get('grapes');
-console.log(myHashTable.get('grapes'));
-
-// console.dir(myHashTable.data);
+myHashTable.set('apples', 10001);
+myHashTable.set('oranges', 9);
+console.log(myHashTable.keys());
