1a12d10757b4ddb0c04e410ccde4cfdcb450388a
william <william.del.rosario@iceshop.nl>
2020-04-05 13:29:26 +0200

    concrete examples

---
+++ problem_solving/2_concrete_example/index.js
@@ -0,0 +1,7 @@
+// write a function which takes in a string and returns counts of each character in the string
+// dummy data
+charCount("aaaa"); // {a:4}
+charCount("hello"); // {h:1, e:2, l:3, l:4, o:5}
+"My phone is 12345";
+// edge cases
+// empty, invalid etc inputs
