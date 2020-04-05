3160846000571c9e7d5a02cffe0b5a73adabd2ab
william <william.del.rosario@iceshop.nl>
2020-04-05 13:40:29 +0200

    break it down

---
+++ problem_solving/3_break_it_down/index.js
@@ -0,0 +1,19 @@
+// write a function which takes in a string and returns counts of each character in the string
+charCount("aaaaa");
+// {a: 4}
+
+function charCount(str) {
+    // do something
+    // return an object with keys that are lowercase alphanumeric characters in the string
+    // values should be the
+}
+
+function charCount() {
+    // make object to return at the end
+    // loop over string
+    // for each character...
+        // if char is number/letter AND is a key in object, add one to count
+        // if char is number/letter AND not in in object, add it to object and set value to 1
+        // if char is something else (spaces. period etc) don't do anything
+    // return object at end
+}
