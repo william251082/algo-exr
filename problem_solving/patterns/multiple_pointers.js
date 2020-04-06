518c29245f55023a9268edf747b5813b84de07e1
william <william.del.rosario@iceshop.nl>
2020-04-06 11:09:03 +0200

    multiple pointers

---
+++ problem_solving/patterns/multiple_pointers.js
@@ -0,0 +1,39 @@
+// function that accepts sorted array of integers.
+// return array containing the first pair where the sum is 0
+// or undefined if pairs don't exist
+// function sumZero(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         for(let j = i+1; j < arr.length; j++){
+//             if(arr[i] + arr[j] === 0){
+//                 return [arr[i], arr[j]];
+//             }
+//         }
+//     }
+// }
+// // time complexity - O(n^2)
+// // space complexity - O(1)
+//
+//
+// console.log(sumZero([-4,-3,-2,-1,0,1,2,5]));
+
+
+// refactor
+function sumZero(arr){
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+        if (sum === 0) {
+            return [arr[left], arr[right]];
+        } else if (sum > 0) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+}
+
+console.log(sumZero([-4,-3,-2,-1,0,1,2,5]));
+
+
+
